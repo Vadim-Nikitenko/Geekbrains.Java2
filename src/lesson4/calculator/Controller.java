8cc4ce0d6e7e142cd179d0e870a8da704dd77682
Vadim-Nikitenko <nikagw24@gmail.com>
2020-05-27 12:11:13 +0300

    Lesson4

--- src/lesson4/calculator/Controller.java
+++ src/lesson4/calculator/Controller.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Font;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
